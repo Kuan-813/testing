617a0ebaeb8d0fcf6b4cce24adf50f8539b4e7a2
Kuannnnn <kuanyoke813@gmail.com>
2026-03-03 10:10:58 +0800

    D2_Update

---
+++ PY/D2_Exercise_01.py
@@ -0,0 +1,7 @@
+text = "Python Programming"
+
+print(text[3])
+print(text[-1])
+print(text[0:6])
+print(text[:6])
+print(text[7:])
---
+++ PY/D2_Exercise_02.py
@@ -0,0 +1,8 @@
+name = "Bob the builder"
+
+print(len(name))
+print(name.strip())
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.replace("Bob","Kim"))
